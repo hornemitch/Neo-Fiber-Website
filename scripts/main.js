1308965ed9230dec3a6a5d66aab9207eda741ffe
hornemitch <mitchhorne92@gmail.com>
2016-08-05 14:08:16 +0200

    Version 1.0

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,34 @@
+/* Global sorting variables */
+var type = "all";;
+/* Global sorting variables */
+
+/* Website init */
 $(document).ready(function(){
+
+    /* Bootstrap tooltip init */
     $('[data-toggle="tooltip"]').tooltip({
+        container: 'body',
         trigger: "hover"
     });
-    var type = "all",
-        line = "all",
-        cap  = "all";
+    /* Bootstrap tooltip init */
 
+    /* Smooth scroll init */
+    $(".nav-link").smoothScroll({
+        speed: 800
+    });
+    /* Smooth scroll init */
+});
+/* Website init */
+
+/* Navigation control */
+$(".nav-control").on("click", function() {
+    operateNav();
+});
+$(".nav-link").on("click", function() {
+    operateNav();
 });
 
+/* Navigation open/close function */
 function operateNav(){
     var nav = $("nav");
 
@@ -19,45 +40,75 @@ function operateNav(){
         nav.addClass("nav-closed");
     }
 }
+/* Navigation open/close function */
+/* Navigation control */
 
-function openTerminology(){
+/* Terminology control */
+$(".terminology-button").on("click", function(){
+    operateTerminology();
+});
+$(".close-terminology").on("click", function(){
+    operateTerminology();
+});
+
+/* Terminology open/close function */
+function operateTerminology(){
     var term = $(".terminology-cover");
 
-    term.removeClass("closed");
-    setTimeout(function(){
-        term.removeClass("slideOutRight");
-        term.addClass("slideInRight");
-    },1200);
+    if(term.hasClass("closed")) {
+        term.removeClass("closed");
+        setTimeout(function () {
+            term.removeClass("slideOutRight");
+            term.addClass("slideInRight");
+        }, 500);
+    } else {
+        term.addClass("closed");
+        setTimeout(function(){
+            term.removeClass("slideInRight");
+            term.addClass("slideOutRight");
+        },500);
+    }
 }
-function closeTerminology(){
-    var term = $(".terminology-cover");
+/* Terminology open/close function */
+/* Terminology control */
 
-    term.addClass("closed");
-    setTimeout(function(){
-        term.removeClass("slideInRight");
-        term.addClass("slideOutRight");
-    },500);
-}
+/* Packages control */
+$(".package-button").on("click", function(){
+    operatePackages();
+});
+$(".close-packages").on("click", function(){
+    operatePackages();
+});
 
-function openPackages(){
+/* Packages open/close function */
+function operatePackages(){
     var packages = $(".packages-sort-cover");
+    var container = $(".packages");
 
-    packages.removeClass("closed");
-    setTimeout(function(){
-        packages.removeClass("slideOutRight");
-        packages.addClass("slideInRight");
-    },1200);
+    if(!container.hasClass("closed")){
+        container.slick("slickUnfilter");
+        container.slick("unslick");
+        container.addClass("closed");
+    }
+
+    if(packages.hasClass("closed")){
+        packages.removeClass("closed");
+        setTimeout(function(){
+            packages.removeClass("slideOutRight");
+            packages.addClass("slideInRight");
+        },500);
+    } else {
+        packages.addClass("closed");
+        setTimeout(function(){
+            packages.removeClass("slideInRight");
+            packages.addClass("slideOutRight");
+        },500);
+    }
 }
-function closePackages(){
-    var packages = $(".packages-sort-cover");
+/* Packages open/close function */
+/* Packages control */
 
-    packages.addClass("closed");
-    setTimeout(function(){
-        packages.removeClass("slideInRight");
-        packages.addClass("slideOutRight");
-    },500);
-}
-
+/* Type selection functions */
 $(".type-button").on("click", function(){
     type = $(this).attr("value");
     var activeButton = $(".type-button.active");
@@ -82,3 +133,43 @@ $(".type-button").on("click", function(){
     $(this).removeClass("package-sort-button");
     $(this).addClass("active");
 });
+/* Type selection functions */
+
+/* Show sorted packages control */
+$(".package-sort-button-search").on("click", function(){
+    showPackages();
+});
+
+/* Show sorted packages function */
+function showPackages(){
+    var container = $(".packages");
+    if(type == "all"){
+        operatePackages();
+        container.removeClass("closed");
+        container.slick({
+            prevArrow: "<i class='fa fa-angle-left prev-button'><i/>",
+            nextArrow: "<i class='fa fa-angle-right next-button'><i/>",
+            speed: 500
+        });
+    } else if(type == "business"){
+        operatePackages();
+        container.removeClass("closed");
+        container.slick({
+            prevArrow: "<i class='fa fa-angle-left prev-button'><i/>",
+            nextArrow: "<i class='fa fa-angle-right next-button'><i/>",
+            speed: 500
+        });
+        container.slick("slickFilter", ".business");
+    } else if(type == "home"){
+        operatePackages();
+        container.removeClass("closed");
+        container.slick({
+            prevArrow: "<i class='fa fa-angle-left prev-button'><i/>",
+            nextArrow: "<i class='fa fa-angle-right next-button'><i/>",
+            speed: 500
+        });
+        container.slick("slickFilter", ".home");
+    }
+}
+/* Show sorted packages function */
+/* Show sorted packages control */
